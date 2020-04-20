399b3fe2f64fce73c48a62d5771a5bacf9945c00
Jeff Day <jeffrey.day00@gmail.com>
2020-04-20 16:40:19 -0700

    Boilerplate installed.

---
+++ client/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// Import react DOM in order to mount our App to the DOM
+import ReactDOM from 'react-dom';
+// Import the App component which is the top level component of our app
+import App from './components/App.jsx';
+
+// Attach App to the DOM, replacing the div with ID app
+ReactDOM.render(<App/>, document.getElementById('app'));
